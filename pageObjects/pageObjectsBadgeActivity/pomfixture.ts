f970558022a70d1cd2751f3d89f1395a80c579a9
allan10carlo86-3cloud <allan.ramos@3cloudsolutions.com>
2026-02-05 20:43:56 +0800

    Update to resolve Adrian's Review Points

--- pageObjects/pageObjectsBadgeActivity/pomfixture.ts
+++ pageObjects/pageObjectsBadgeActivity/pomfixture.ts
@@ -4,12 +4,14 @@ import {test as baseTest} from '@playwright/test';
 import _homePage from './homePage.ts';
 import _financialServicesPage from './financialServicesPage.ts';
 import _contactUsPage from './contactUsPage.ts'; 
+import _featureFile from './featureFile.ts';
 
 
 type pages = {
     p_homePage : _homePage,
     p_financialServicesPage : _financialServicesPage,
-    p_contactUsPage : _contactUsPage
+    p_contactUsPage : _contactUsPage,
+    p_featureFile : _featureFile
 }
 
 const testPages = baseTest.extend<pages>({
@@ -23,7 +25,14 @@ const testPages = baseTest.extend<pages>({
 
     p_contactUsPage : async ({page}, use) => {
         await use (new _contactUsPage(page));
+    },
+
+    p_featureFile : async ({page}, use) => {    
+        await use (new _featureFile(page));
     }
 })  
 
 export const test = testPages;
+
+
+
--- tests/badgeActivity/TEST00001_enterValuesUnderContactUsWithError.test.ts
+++ tests/badgeActivity/TEST00001_enterValuesUnderContactUsWithError.test.ts
@@ -1,15 +1,6 @@
 import {test} from "../../pageObjects/pageObjectsBadgeActivity/pomfixture.ts";
 
 
-test('Enter the Contact Us Page under the Financial Services with Error Generation', async ({ page, p_homePage, p_contactUsPage}) => {
-    await page.goto("https://3cloudsolutions.com/");    
-    await p_homePage.navigateToWhoWeAre();
-    await p_homePage.navigateAndClickToFinancialServices();
-    await p_contactUsPage.enterValuesUnderContactUsWithError(
-        "John", 
-        "Doe", 
-        "johndoe@ibm.com", 
-        "Example Corp", 
-        "Manager", 
-        "This is a test message."  );
+test('Enter the Contact Us Page under the Financial Services with Error Generation', async ({ page, p_featureFile, p_homePage, p_contactUsPage }) => {
+    await p_featureFile.FillOutContactFormWithError({page, p_homePage, p_contactUsPage});
 });
